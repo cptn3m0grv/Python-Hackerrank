2a8bc287960691407a6e896834c076314d490be5
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-09-17 09:43:00 +0530

    nested list error corrected

--- Basic Data Types/nested-list.py
+++ Basic Data Types/nested-list.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
         score = float(input())
         ls.append([name, score])
     
-    second_highest = sorted([marks for name, marks in ls])[1]
+    second_highest = sorted(list(set([marks for name, marks in ls])))[1]
 
     for name, marks in sorted(ls):
         if marks == second_highest:
